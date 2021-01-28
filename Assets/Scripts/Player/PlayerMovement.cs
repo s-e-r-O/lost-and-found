b0e509ec1de884a2d778a7926a53d9936ac79a90
Sergio Oporto <ser.op.te@gmail.com>
2021-01-28 00:03:29 -0400

    Add base usage of networking

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            transform.position = transform.position + new Vector3(dir.x, dir.y, 0) * speed * Time.deltaTime;
+        }
+    }
+
+    void Update()
+    {
+        HandleMovement();
+    }
+}
