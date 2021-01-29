c62a3c08b7020f483e35107aaaadb341c6e3048d
Sergio Oporto <ser.op.te@gmail.com>
2021-01-28 23:03:13 -0400

    Add grab action

---
+++ Assets/ItemPlayer.cs
@@ -0,0 +1,21 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPlayer : NetworkBehaviour
+{
+    [ServerCallback]
+    public void Collect()
+    {
+        Debug.Log("Someone collected", gameObject);
+        var identity = GetComponent<NetworkIdentity>();
+        Collected();
+    }
+
+    [ClientRpc]
+    public void Collected()
+    {
+        gameObject.transform.position = Vector3.zero;
+    }
+}
