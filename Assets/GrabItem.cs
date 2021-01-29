fa5988ed53f6198dc6789aed085eaadac511ca0f
Sergio Oporto <ser.op.te@gmail.com>
2021-01-28 23:03:13 -0400

    Add grab action

---
+++ Assets/GrabItem.cs
@@ -0,0 +1,57 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabItem : NetworkBehaviour
+{
+    [SerializeField]
+    LayerMask itemMask;
+    [SerializeField]
+    Transform grabTransform;
+    [SerializeField]
+    float grabRadius;
+    private void Update()
+    {
+        if (isLocalPlayer)
+        {
+            CheckNearItems();
+        }
+    }
+    
+    [ClientCallback]
+    void CheckNearItems()
+    {
+        var cols = Physics.OverlapSphere(grabTransform.position, grabRadius, itemMask);
+        if (cols.Length > 0)
+        {
+            Debug.Log($"I got some ({cols.Length})");
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                foreach (var col in cols)
+                {
+                    CollectItem(col.GetComponentInParent<ItemPlayer>().gameObject);
+                }
+            }
+        }
+    }
+    [Command]
+    void CollectItem(GameObject item)
+    {
+        item.GetComponent<ItemPlayer>().Collect();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(grabTransform.position, grabRadius);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        var item = other.GetComponentInParent<ItemPlayer>();
+        if (item != null)
+        {
+            Debug.Log($"Got 1 item!! {item.gameObject.name}");
+        }
+    }
+}
