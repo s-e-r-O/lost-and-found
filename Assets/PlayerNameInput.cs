c623c7023b412dc65f136208318ce01bb6e6cb6e
Sergio Oporto <ser.op.te@gmail.com>
2021-01-30 13:05:14 -0400

    Add main menu controls

---
+++ Assets/PlayerNameInput.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameInput : MonoBehaviour
+{
+    [SerializeField] TMP_InputField playerInputField;
+    [SerializeField] Button[] disabledButtons;
+
+    public string DisplayName { get; private set; }
+    private const string PlayerPrefsNameKey = "PlayerName";
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpInputField();
+    }
+
+    private void SetUpInputField()
+    {
+        //if (!PlayerPrefs.HasKey(PlayerPrefsNameKey)) { return; }
+
+        var displayName = PlayerPrefs.GetString(PlayerPrefsNameKey, "");
+
+        playerInputField.text = displayName;
+        SetPlayerName(displayName);
+
+    }
+
+    public void SetPlayerName(string name)
+    {
+        foreach(var button in disabledButtons)
+        {
+            button.interactable = !string.IsNullOrEmpty(name);
+        }
+    }
+    public void SavePlayerName()
+    {
+        DisplayName = playerInputField.text;
+        PlayerPrefs.SetString(PlayerPrefsNameKey, DisplayName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
