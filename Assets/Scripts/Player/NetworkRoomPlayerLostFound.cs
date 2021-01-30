f425b9cb69dd34a0a78f073222ac72d650332fc9
Sergio Oporto <ser.op.te@gmail.com>
2021-01-30 14:07:23 -0400

    First steps for lobby

---
+++ Assets/Scripts/Player/NetworkRoomPlayerLostFound.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkRoomPlayerLostFound : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
