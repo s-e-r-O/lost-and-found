ba966fe4a9a260fd1918f85a3bda9a6159168d97
Aldo Oporto <a_r_oporto@outlook.com>
2021-01-31 16:04:55 -0400

    Added audio files

--- Assets/PlayerCollisionHandler.cs
+++ Assets/PlayerCollisionHandler.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class PlayerCollisionHandler : MonoBehaviour
 {
     [SerializeField] private NetworkGamePlayerLostFound player;
+    public LayerMask bushLayer;
     private void OnCollisionEnter(Collision collision)
+    { 
+        PlayerCollisionHandler other = collision.gameObject.GetComponent<PlayerCollisionHandler>();
+        if (other == null) 
+        {
+            AudioManager.Instance.Play("Hit");
+            return; 
+        }
+        player.OnPlayerCollision(other.player);
+        AudioManager.Instance.Play("Caught");
+    }
+
+    private void OnTriggerEnter(Collider collision)
     {
         PlayerCollisionHandler other = collision.gameObject.GetComponent<PlayerCollisionHandler>();
-        if (other == null) { return; }
-        player.OnPlayerCollision(other.player);
+
+        if (bushLayer == (bushLayer | (1 << collision.gameObject.layer)))
+            AudioManager.Instance.Play("Bush");
     }
 }
