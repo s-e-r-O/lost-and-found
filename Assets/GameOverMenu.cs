71e0ca330880667ef8cb8d7e034f545cb22f4629
Sergio Oporto <ser.op.te@gmail.com>
2021-01-31 15:03:04 -0400

    Fix game over animation

--- Assets/GameOverMenu.cs
+++ Assets/GameOverMenu.cs
@@ -8,6 +8,7 @@ public class GameOverMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text finders;
     [SerializeField] private TMP_Text items;
+    private Animator anim;
     private NetworkManagerLostFound room;
     private NetworkManagerLostFound Room
     {
@@ -18,10 +19,17 @@ public class GameOverMenu : MonoBehaviour
         }
     }
 
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     public void SetWinner(string winner)
     {
         finders.gameObject.SetActive(winner == "FINDER");
         items.gameObject.SetActive(winner == "ITEM");
+        anim.ResetTrigger("GameOver");
+        anim.SetTrigger("GameOver");
     }
 
     public void Back()
