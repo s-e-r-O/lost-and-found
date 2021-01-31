341eb42572154ed2e5def44142083d2f05869e6d
Sergio Oporto <ser.op.te@gmail.com>
2021-01-30 23:07:49 -0400

    Add lobby

--- Assets/Scripts/Core/NetworkManagerLostFound.cs
+++ Assets/Scripts/Core/NetworkManagerLostFound.cs
@@ -8,6 +8,7 @@ using UnityEngine.SceneManagement;
 
 public class NetworkManagerLostFound : NetworkManager
 {
+    [SerializeField] private int minPlayers = 2;
     [SerializeField] private string menuScene = string.Empty;
 
     [Header("Room")]
@@ -15,6 +16,8 @@ public class NetworkManagerLostFound : NetworkManager
 
     public static event Action OnClientConnected;
     public static event Action OnClientDisconnected;
+
+    public List<NetworkRoomPlayerLostFound> RoomPlayers { get; } = new List<NetworkRoomPlayerLostFound>();
 
     public override void OnClientConnect(NetworkConnection conn)
     {
@@ -46,8 +49,46 @@ public class NetworkManagerLostFound : NetworkManager
     {
         if (SceneManager.GetActiveScene().name == menuScene)
         {
+            bool isLeader = RoomPlayers.Count == 0;
             NetworkRoomPlayerLostFound roomPlayerInstance = Instantiate(roomPlayerPrefab);
+            roomPlayerInstance.IsLeader = isLeader;
             NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
         }
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        if (conn.identity != null)
+        {
+            var player = conn.identity.GetComponent<NetworkRoomPlayerLostFound>();
+            RoomPlayers.Remove(player);
+            NotifyPlayersOfReadyState();
+        }
+        base.OnServerDisconnect(conn);
+    }
+
+    public override void OnStopServer()
+    {
+        RoomPlayers.Clear();
+    }
+
+    public void NotifyPlayersOfReadyState()
+    {
+        foreach(var player in RoomPlayers)
+        {
+            player.HandleReadyToStart(IsReadyToStart());
+        }
+    }
+    public bool IsReadyToStart()
+    {
+        if (numPlayers < minPlayers) { return false; }
+        int finders = 0;
+        int items = 0;
+        foreach (var player in RoomPlayers)
+        {
+            if (player.PlayerType == "FINDER") { finders++; }
+            if (player.PlayerType == "ITEM") { items++; }
+        }
+        return finders > 0 && items > 0 && (finders + items) == numPlayers;
     }
 }
